6dc2b92ad481a9ea6c161dd213889a0ffd47b2a6
Eduard088 <56804496+Eduard088@users.noreply.github.com>
2026-01-28 15:02:27 -0600

    Add files via upload

---
+++ settings/settings.py
@@ -0,0 +1,10 @@
+owid_palette = [
+    "#4e79a7", "#f28e2b", "#e15759", 
+    "#76b7b2", "#59a14f", "#edc948", "#b07aa1"
+]
+
+gender_palette = {
+    "Mujer": "violet", 
+    "Hombre": "skyblue",
+    "No especificado": "red"
+}
